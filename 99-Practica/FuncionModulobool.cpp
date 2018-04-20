ce6dc750c91f3300cbe78c197633947611e3dcf8
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2018-04-20 19:05:30 -0300

    Mejorar formato cpp

--- 99-Practica/FuncionModulobool.cpp
+++ 99-Practica/FuncionModulobool.cpp
@@ -3,17 +3,13 @@
 
 unsigned modulo (int);
 unsigned modulo (int a){
-	
 	bool b (a>0);
 	bool c (a<0);
    return a * (b-c);
-
-}
+   }
 
 int main() {
-
     assert (4==modulo(4));
     assert (4==modulo(-4));
     assert (0==modulo(0));
-
-}
+    }
