f50c446918801f8f70acdf4d93eddbddefd7d9fb
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2018-04-26 20:10:47 -0300

    Corrección de forma

--- 99-Practica/FuncionModulo.cpp
+++ 99-Practica/FuncionModulo.cpp
@@ -7,11 +7,13 @@
 #include <cassert>
 
 unsigned modulo (int);
-unsigned modulo (int a){
-   return a * ((a>0)-(a<0));
-}
+
 int main() {
     assert (4==modulo(4));
     assert (4==modulo(-4));
     assert (0==modulo(0));
+}
+
+unsigned modulo (int a){
+   return a * ((a>0)-(a<0));
 }
